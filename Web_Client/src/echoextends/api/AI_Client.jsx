23a6934f7bd5688e58c6771f3c2f25ce84a64b63
Itto Kawamata <kawanuuto.1012@gmail.com>
2026-06-16 20:29:41 +0900

    updae

--- Web_Client/src/echoextends/api/AI_Client.jsx
+++ Web_Client/src/echoextends/api/AI_Client.jsx
@@ -1,10 +1,13 @@
-// const API_URL = "https://your-render-url.onrender.com/ai/move";
-const API_URL = "http://localhost:8000/ai/move";
+const API_URL = "https://echoextends.onrender.com";
+//const API_URL = "http://localhost:8000";
+
 export async function requestAIMove(board, turn, aiType) {
-  const res = await fetch(API_URL, {method: "POST",
+  const res = await fetch(`${API_URL}/ai/move`, {
+    method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ board, turn, aiType }),
   });
 
   return await res.json();
 }
+
